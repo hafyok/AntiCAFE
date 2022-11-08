56805ce8b669c212754f0fd6975372f92e923dc0
Haggai Khafizov <hafyok5777@gmail.com>
2022-11-08 11:59:25 +0300

    Adding an event to the calendar

--- app/src/main/java/com/example/anticafe/MainActivity.java
+++ app/src/main/java/com/example/anticafe/MainActivity.java
@@ -7,16 +7,21 @@ import androidx.room.Room;
 import android.app.Application;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 import com.example.anticafe.Database.AppDatabase;
 import com.example.anticafe.Database.AppRepository;
 import com.example.anticafe.R;
+import com.example.anticafe.databinding.ActivityMainBinding;
+import com.example.anticafe.databinding.RoomsFragmentBinding;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RoomsFragmentBinding binding;
     AppRepository appRepository;
 
     @Override
@@ -27,5 +32,11 @@ public class MainActivity extends AppCompatActivity {
         ServiceLocator.init(getApplicationContext());
 
     }
+
+    /*public void addEvent(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState){
+        binding = RoomsFragmentBinding.inflate(inflater, container, false);
+        View v = binding.getRoot();
+    }*/
 
 }
--- app/src/main/java/com/example/anticafe/View/RoomsFragment.java
+++ app/src/main/java/com/example/anticafe/View/RoomsFragment.java
@@ -2,6 +2,7 @@ package com.example.anticafe.View;
 
 import static java.util.stream.Collectors.toList;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -11,29 +12,46 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.anticafe.MainActivity;
 import com.example.anticafe.Model.Employee;
 import com.example.anticafe.ViewModels.RoomsViewModel;
 import com.example.anticafe.databinding.RoomsFragmentBinding;
 
 import com.example.anticafe.View.adapters.RoomsAdapter;
 
+import java.util.Calendar;
+
 public class RoomsFragment extends Fragment {
     private RoomsViewModel mViewModel;
     RoomsFragmentBinding binding;
+    Button btnAddEvent;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = RoomsFragmentBinding.inflate(inflater, container, false);
+
+        btnAddEvent = binding.btnAddEvent;
+
+        btnAddEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addEvent();
+            }
+        });
+
         View v = binding.getRoot();
 
         binding.RoomsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
         return v;
         //return inflater.inflate(R.layout.rooms_fragment, container, false);
     }
@@ -53,5 +71,33 @@ public class RoomsFragment extends Fragment {
             );
         }
         );
+    }
+
+    public void addEvent(){
+        Log.d("Calendar", "Begin func");
+        /*Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+        Calendar beginTime = Calendar.getInstance().set(2012, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance().set(2012, 0, 19, 10, 30);
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        calendarIntent.putExtra(CalendarContract.Events.TITLE, "Testsss");
+        calendarIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Locationnn");*/
+        Calendar cal = Calendar.getInstance();
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra("beginTime", cal.getTimeInMillis());
+        intent.putExtra("allDay", true);
+        intent.putExtra("rrule", "FREQ=YEARLY");
+        intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
+        intent.putExtra("title", "Бронь комнаты");
+        startActivity(intent);
+        Log.d("Calendar", "End func");
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
